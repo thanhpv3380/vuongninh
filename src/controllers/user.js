9542d8fb27e2e052360ce43787a85e65afccc907
henry <thanhpv3380@gmail.com>
2024-09-28 10:19:31 +0700

    Update create public folder

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 const userService = require('../services/user');
 
-const subscribe = async (req) => userService.subscribe(req.body);
+const subscribe = async (req) => userService.subscribe(req.body, req.requestId);
 
 module.exports = {
   subscribe,
--- src/services/init.js
+++ src/services/init.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
 const logger = require('../utils/logger');
 
 global.logger = logger;
+
+try {
+  fs.mkdirSync(path.join(__dirname, '..', '../', 'public'), { recursive: true });
+} catch (err) {
+  logger.error(`Init Service | Error creating directory: ${err.message}`);
+}
